f51a4571f8e2bd461fdd97e6608a28e228bd6249
hsx <444012836@qq.com>
2019-11-13 11:11:13 +0800

    this is test git update to remote github!

--- src/com/Test.java
+++ src/com/Test.java
@@ -3,46 +3,69 @@ package com;
 /**
  * @Author HLX
  * @Date 2019/11/11 14:49
- * @conent:  素数的定义是指在除了1和此整数本身外,不能被其他自然数整除的数（1除外）。
+ * @conent: 素数的定义是指在除了1和此整数本身外, 不能被其他自然数整除的数（1除外）。
  */
 public class Test {
     public static void main(String[] args) {
 //        two();
-                  boolean flag=false;
-            //1-100之间的质数
-            for (int i = 2; i <101 ; i++) {
-                for (int j = 2; j < i; j++) {
-                    if(i%j==0){
-                        flag=true;
-                        break;
-                    }
-                }
-                //i is
-                if(!flag){
-                    System.out.print(i+"\t");
-                }
-                //change flag value false
-                flag=false;
-        }
+//         three();
+        parallelogram();
 
     }
 
-    private static void two() {
-        boolean flag=false;
+    private static void three() {
+        boolean flag = false;
         //1-100之间的质数
-        for (int i = 2; i <101 ; i++) {
-            for (int j = 2; j<=Math.sqrt(i) ; j++) {
-                if(i%j==0){ //i equals j value zero
-                    flag=true;
+        for (int i = 2; i < 101; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            //i is
+            if (!flag) {
+                System.out.print(i + "\t");
+            }
+            //change flag value false
+            flag = false;
+        }
+    }
+
+    private static void two() {
+        boolean flag = false;
+        //1-100之间的质数
+        for (int i = 2; i < 101; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) { //i equals j value zero
+                    flag = true;
                     break;
                 }
             }
             //质数(素数)
-            if(!flag){
-                System.out.print(i+"\t");
+            if (!flag) {
+                System.out.print(i + "\t");
             }
             //reset flag value false;
-            flag=false;
+            flag = false;
+        }
+    }
+
+    /**
+     * print parallelogram
+     */
+    private static void parallelogram() {
+        for (int i = 0; i < 3; i++) {
+            //打印空格
+            for (int j = 0; j < 4 - i - 1; j++) {
+                System.out.print(" ");
+            }
+            //打印*
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            //换行
+            System.out.println();
         }
     }
 }
---
+++ src/net/Test1.java
@@ -0,0 +1,11 @@
+package net;
+
+/**
+ * @Author HLX
+ * @Date 2019/11/13 10:53
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("this is a new table!");
+    }
+}
